372736c336b480c362aa30fa3fb9c1f554a19af1
Ivan Dubrovin <ivan.dubrovin@proton.me>
2023-05-27 14:10:55 +0300

    Add centering to scrolling Neovim commands

--- nvim/lua/user/remap.lua
+++ nvim/lua/user/remap.lua
@@ -1,3 +1,7 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)  -- file explorer
+
+-- Center the screen after scrolling with C-d and C-u
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
