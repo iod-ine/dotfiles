0bf8abfcae7fd54e869c13408060ee482f4debe2
Ivan Dubrovin <ivan.dubrovin@proton.me>
2023-05-27 20:53:02 +0300

    Make Neovim background transparent

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,3 +4,7 @@ require("user.remap")
 
 -- Set the colorscheme
 vim.cmd.colorscheme("kanagawa-wave")
+
+-- Make the background transparent
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
