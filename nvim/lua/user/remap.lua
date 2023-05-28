88bbe2f4425ffd7eeb8ac8ab9abbd7276253f0e2
Ivan Dubrovin <ivan.dubrovin@proton.me>
2023-05-28 12:27:33 +0300

    Set bindings for buffer related Neovim commands

--- nvim/lua/user/remap.lua
+++ nvim/lua/user/remap.lua
@@ -5,3 +5,9 @@ vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)  -- file explorer
 -- Center the screen after scrolling with C-d and C-u
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Buffer manipulation commands
+vim.keymap.set("n", "<leader>bl", vim.cmd.ls)         -- buffer list
+vim.keymap.set("n", "<leader>bn", vim.cmd.bnext)      -- buffer next
+vim.keymap.set("n", "<leader>bp", vim.cmd.bprevious)  -- buffer previus
+vim.keymap.set("n", "<leader>bc", vim.cmd.bdelete)    -- buffer close
