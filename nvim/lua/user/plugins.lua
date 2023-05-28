976ea045ab8136ca62b301a292911000a7273dee
Ivan Dubrovin <ivan.dubrovin@proton.me>
2023-05-28 10:28:04 +0300

    Install telescope.nvim fuzzy finder

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -2,6 +2,15 @@ return require("packer").startup(function(use)
   -- Packer can manage itself
   use("wbthomason/packer.nvim")
 
+  -- Fuzzy finder. :Telescope is the intry point
+  use({
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    requires = {
+        {"nvim-lua/plenary.nvim"},
+    },
+  })
+
   -- Colorschemes
   use("rebelot/kanagawa.nvim")
   use("folke/tokyonight.nvim")
