e7e8f18f90606f4aab8646c50cf35db5fa41c2b2
Ivan Dubrovin <ivan.dubrovin@proton.me>
2023-09-29 09:31:04 +0300

    Switch from pyright to python-lsp-server

--- nvim/lua/user/lsp.lua
+++ nvim/lua/user/lsp.lua
@@ -1,2 +1,15 @@
 local lspconfig = require("lspconfig")
-lspconfig.pyright.setup({})
+
+-- Python LSP Server
+-- https://github.com/python-lsp/python-lsp-server/blob/develop/CONFIGURATION.md
+lspconfig.pylsp.setup({
+    settings = {
+        pylsp = {
+            configurationSources = {"flake8"},  -- Load the default configuration
+            plugins = {
+                pycodestyle = { enabled = false },
+                flake8 = { enabled = true },
+            },
+        },
+    },
+})
