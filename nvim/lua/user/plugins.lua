0c199d88c15857674d7f8a2e04e47a88fb47e851
Ivan Dubrovin <ivan.dubrovin@proton.me>
2023-10-10 14:58:07 +0300

    Fix the build command for fzf Telescope sorter
    The one without the space in -S . produced an error:
      CMake Error: The source directory "" does not exist.

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -22,7 +22,7 @@ require("lazy").setup({
     {
         "nvim-telescope/telescope-fzf-native.nvim",
         build = [[
-            cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && \
+            cmake -S . -Bbuild -DCMAKE_BUILD_TYPE=Release && \
             cmake --build build --config Release && \
             cmake --install build --prefix build
         ]],
