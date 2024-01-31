8035e615de7162e3ac96f052d6157e11fe051e26
Ivan Dubrovin <ivan.dubrovin@proton.me>
2024-01-31 20:59:18 +0300

    Create an autocommand to open all folds

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -12,6 +12,9 @@ vim.opt.smartindent = true
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
+local group = vim.api.nvim_create_augroup("open_all_folds", { clear = true })
+vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost" }, { command = "normal zR", group = group })
+
 vim.opt.wrap = false
 vim.opt.scrolloff = 8
 
