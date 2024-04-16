634885ee0f8ff0fea1164916b3c5481a1a856f13
Ivan Dubrovin <ivan.dubrovin@proton.me>
2024-04-16 17:43:00 +0300

    Enable smartcase for search in Neovim

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -30,6 +30,7 @@ vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 vim.opt.termguicolors = true
 
