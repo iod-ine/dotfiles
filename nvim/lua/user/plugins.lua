74803cb52cafcd65440ab36fb14e4824d5adebef
Ivan Dubrovin <ivan.dubrovin@proton.me>
2024-05-08 14:40:23 +0300

    Move activation of treesitter plugin

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -35,6 +35,19 @@ require("lazy").setup({
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
+        opts = {
+            ensure_installed = {
+                "c",
+                "lua",
+                "vim",
+                "vimdoc",
+                "query",
+                -- These five should always be installed
+            },
+            sync_install = false,
+            auto_install = false,
+            highlight = { enable = true },
+        }
     },
     { "nvim-treesitter/nvim-treesitter-context", opts = {} },
 
