4828e8ca604252933ec1e0240e9c281ccfa53932
Ivan Dubrovin <ivan.dubrovin@proton.me>
2024-07-02 20:27:59 +0300

    Disable showing mode
    It is shown in the status line already.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,6 +3,8 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+vim.opt.showmode = false
+
 vim.opt.tabstop = 4          -- Visual width of a tab character
 vim.opt.softtabstop = 4      -- How much whitespace Tab key inserts
 vim.opt.shiftwidth = 4       -- How much whitespace to use for indentation commands
