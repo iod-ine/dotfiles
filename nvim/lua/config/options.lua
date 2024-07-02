0139b0e0606a91343725f3be49a1f7db878154c4
Ivan Dubrovin <ivan.dubrovin@proton.me>
2024-07-02 09:55:49 +0300

    Enable visualization of whitespaces

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -13,6 +13,8 @@ vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldenable = false   -- Disable folds by default, toggle with zi
 
+vim.opt.list = true
+
 vim.opt.wrap = false
 vim.opt.scrolloff = 8
 
