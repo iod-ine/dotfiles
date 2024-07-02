b7cf56822bea9405a6c3f8d58c97c94af7ae54fa
Ivan Dubrovin <ivan.dubrovin@proton.me>
2024-07-02 10:08:18 +0300

    Add autocmd to LSP highlight symbol under cursor

--- nvim/lua/config/autocommands.lua
+++ nvim/lua/config/autocommands.lua
@@ -5,3 +5,18 @@ vim.api.nvim_create_autocmd("TextYankPost", {
         vim.highlight.on_yank()
     end,
 })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("lsp-autocmds", { clear = true }),
+    callback = function()
+        vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+            pattern = { "*.py", "*.lua", "*.c", "*.cpp" },
+            callback = vim.lsp.buf.document_highlight,
+        })
+
+        vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+            pattern = { "*.py", "*.lua", "*.c", "*.cpp" },
+            callback = vim.lsp.buf.clear_references,
+        })
+    end,
+})
