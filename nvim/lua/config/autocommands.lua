74704cfb317fc4b6260f4ff9b63b675f79a694d1
Ivan Dubrovin <ivan.dubrovin@proton.me>
2024-07-02 11:28:23 +0300

    Add a reference to the autocommand doc

--- nvim/lua/config/autocommands.lua
+++ nvim/lua/config/autocommands.lua
@@ -1,9 +1,9 @@
+-- :help lua-guide-autocommands
+
 vim.api.nvim_create_autocmd("TextYankPost", {
     desc = "Highlight when yanking (copying) text",
     group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+    callback = function() vim.highlight.on_yank() end,
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
