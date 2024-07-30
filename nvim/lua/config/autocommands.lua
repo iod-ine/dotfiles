7b5142ec1580843014dd2bd86e2e5dc77cd10d39
Ivan Dubrovin <ivan.dubrovin@proton.me>
2024-07-30 18:07:24 +0300

    Check for server capabilities in LSP lsp-autocmds

--- nvim/lua/config/autocommands.lua
+++ nvim/lua/config/autocommands.lua
@@ -9,14 +9,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 vim.api.nvim_create_autocmd("LspAttach", {
     group = vim.api.nvim_create_augroup("lsp-autocmds", { clear = true }),
     callback = function()
-        vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-            pattern = { "*.py", "*.lua", "*.c", "*.cpp" },
-            callback = vim.lsp.buf.document_highlight,
-        })
-
-        vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-            pattern = { "*.py", "*.lua", "*.c", "*.cpp" },
-            callback = vim.lsp.buf.clear_references,
-        })
+        local clients = vim.lsp.get_active_clients()
+        for _, client in ipairs(clients) do
+            if client.supports_method("textDocument/documentHighlight") then
+                vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+                    callback = vim.lsp.buf.document_highlight,
+                })
+                vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+                    callback = vim.lsp.buf.clear_references,
+                })
+                break
+            end
+        end
     end,
 })
