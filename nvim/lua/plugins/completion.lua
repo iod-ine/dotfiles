8683c1289be0c013e6c5c943c1350a91f3f5ee7a
Ivan Dubrovin <ivan.dubrovin@proton.me>
2024-09-19 19:35:13 +0300

    Install and set up the Codeium plugin
    Want to give it a try. See whether I am actually going to be replaced by
    an autocomplete on steroids soon. Or if it makes me more fast at coding.
    Or if it makes me dumber at coding.

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -5,4 +5,34 @@ return {
     "hrsh7th/cmp-nvim-lsp",     -- Built-in LSP client
     "saadparwaiz1/cmp_luasnip", -- Snippets
     "onsails/lspkind.nvim",     -- Formatting for the completion window
+    {
+        "Exafunction/codeium.vim",
+        init = function()
+            vim.g.codeium_enabled = false
+            vim.g.codeium_disable_bindings = true
+            vim.g.codeium_idle_delay = 500
+            vim.g.codeium_manual = true
+        end,
+        config = function()
+            local which_key = require("which-key")
+
+            which_key.add(
+                {
+                    { "<leader>ta", "<cmd>CodeiumToggle<cr>", desc = "toggle Codeium" },
+                    {
+                        -- It was very annoying to figure out how to set these mappings up.
+                        -- And the way they need to be set up is also annoying :)
+                        mode = "i",
+                        expr = true,
+                        { "<M-e>", function() return vim.fn["codeium#Accept"]() end },
+                        { "<M-l>", function() return vim.fn["codeium#AcceptNextLine"]() end },
+                        { "<M-n>", function() return vim.fn["codeium#CycleOrComplete"]() end },
+                        { "<M-p>", function() return vim.fn["codeium#CycleCompletions"](-1) end },
+                        { "<M-w>", function() return vim.fn["codeium#AcceptNextWord"]() end },
+                        { "<M-x>", function() return vim.fn["codeium#Clear"]() end },
+                    },
+                }
+            )
+        end,
+    },
 }
