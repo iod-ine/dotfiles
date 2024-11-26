040857a1bcaf1ee0e5533fc0d958ad6d3fda9a62
Ivan Dubrovin <ivan.dubrovin@proton.me>
2024-11-26 21:22:43 +0300

    Configure wizterm window appearance

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,4 +3,9 @@ local config = {}
 
 config.color_scheme = "Afterglow"
 
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.95
+config.macos_window_background_blur = 10
+
 return config
