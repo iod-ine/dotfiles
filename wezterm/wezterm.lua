ec7fb3bf779301cecb837c7744dfa2b9b60e15c6
Ivan Dubrovin <ivan.dubrovin@proton.me>
2024-11-27 13:30:13 +0300

    Set tmux-like bindings for splitting in WezTerm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,4 +9,23 @@ config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.95
 config.macos_window_background_blur = 10
 
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
+config.keys = {
+    {
+        key = "a",
+        mods = "LEADER|CTRL",
+        action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
+    },
+    {
+        key = "|",
+        mods = "LEADER",
+        action = wezterm.action.SplitHorizontal,
+    },
+    {
+        key = "-",
+        mods = "LEADER",
+        action = wezterm.action.SplitVertical,
+    },
+}
+
 return config
