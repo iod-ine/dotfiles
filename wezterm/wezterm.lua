8368910df127fa20ee05fcaa771d6b1e97bdd6f8
Ivan Dubrovin <ivan.dubrovin@proton.me>
2024-11-27 09:49:19 +0300

    Bring back the tab bar in Wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,7 +3,8 @@ local config = {}
 
 config.color_scheme = "Afterglow"
 
-config.enable_tab_bar = false
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.95
 config.macos_window_background_blur = 10
