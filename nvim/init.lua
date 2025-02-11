49ceec8c12e04017a17ca88738ccfc23a761f19a
Ivan Dubrovin <books.booksovich@yandex.com>
2025-02-11 09:32:57 +0300

    Remove old completion configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,7 +3,6 @@ vim.g.mapleader = " "
 require("config.lazy")
 require("config.options")
 require("config.remap")
-require("config.completion")
 require("config.filetypes")
 require("config.lsp")
 require("config.autocommands")
