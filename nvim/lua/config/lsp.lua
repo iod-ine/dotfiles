11617489420cbee25f4ae55f255c24b02e2419a7
Ivan Dubrovin <books.booksovich@yandex.com>
2025-03-29 09:54:12 +0300

    Enable built-in LSP autocompletion

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -17,3 +17,16 @@ vim.api.nvim_create_autocmd("LspAttach", {
         end
     end,
 })
+
+vim.cmd("set completeopt+=noselect")
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    desc = "LSP autocompletion",
+    group = vim.api.nvim_create_augroup("lsp-autocompletion", { clear = true }),
+    callback = function(event)
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+        if client:supports_method("textDocument/completion") then
+            vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
+        end
+    end,
+})
