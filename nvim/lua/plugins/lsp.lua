6963e2ce51cc279cdaf4a4a603aaa01bb8ce2c9f
Ivan Dubrovin <books.booksovich@yandex.com>
2025-03-29 09:46:39 +0300

    Drop nvim-lspconfig plugin

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,15 +1,4 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = { "saghen/blink.cmp" },
-        config = function()
-            local lspconfig = require("lspconfig")
-            local capabilities = require("blink.cmp").get_lsp_capabilities()
-
-            lspconfig.gopls.setup({ capabilities = capabilities })
-            lspconfig.ccls.setup({ capabilities = capabilities })
-        end
-    },
     -- LSP status updates
     {
         "j-hui/fidget.nvim",
