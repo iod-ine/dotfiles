69c8745ca7c508c9af5e1ccb971ea14dbef0d140
Ivan Dubrovin <books.booksovich@yandex.com>
2025-03-29 09:53:04 +0300

    Move LSP symbol highlight autocmd to LSP config

--- nvim/lua/config/autocommands.lua
+++ nvim/lua/config/autocommands.lua
@@ -5,21 +5,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
     callback = function() vim.highlight.on_yank() end,
 })
-
-vim.api.nvim_create_autocmd("LspAttach", {
-    desc = "Highlight symbol under cursor",
-    group = vim.api.nvim_create_augroup("lsp-autocmds", { clear = true }),
-    callback = function(event)
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client.supports_method("textDocument/documentHighlight") then
-            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-                callback = vim.lsp.buf.document_highlight,
-                buffer = event.buf,
-            })
-            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-                callback = vim.lsp.buf.clear_references,
-                buffer = event.buf,
-            })
-        end
-    end,
-})
--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,1 +1,19 @@
 vim.lsp.enable({ "lua_ls", "pylsp", "ruff" })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    desc = "Highlight symbol under cursor",
+    group = vim.api.nvim_create_augroup("lsp-highlight-symbol", { clear = true }),
+    callback = function(event)
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+        if client:supports_method("textDocument/documentHighlight") then
+            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+                callback = vim.lsp.buf.document_highlight,
+                buffer = event.buf,
+            })
+            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+                callback = vim.lsp.buf.clear_references,
+                buffer = event.buf,
+            })
+        end
+    end,
+})
