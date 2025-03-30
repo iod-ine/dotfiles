9e7d083efc5c553970d4b9cf52f01128f10e0699
Ivan Dubrovin <books.booksovich@yandex.com>
2025-03-30 09:58:22 +0300

    Set up lspkind.nvim to format the completion window

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,3 +1,12 @@
 return {
-    "onsails/lspkind.nvim", -- Formatting for the completion window
+    -- Formatting for the completion window
+    {
+        "onsails/lspkind.nvim",
+        config = function()
+            require("lspkind").init({
+                mode = "text_symbol",
+                preset = "default",
+            })
+        end
+    }
 }
