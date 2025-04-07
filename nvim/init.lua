788d26534439ea4fa6827ef44b2f5b91af14a89b
Ivan Dubrovin <books.booksovich@yandex.com>
2025-04-07 10:28:46 +0300

    Rename the Neovim keymap module

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 
 require("config.lazy")
 require("config.options")
-require("config.remap")
+require("config.keymap")
 require("config.filetypes")
 require("config.lsp")
 require("config.autocommands")
