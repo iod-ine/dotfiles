84f8c3934bbbae05d63b44d8a7d0016c6aced66f
Ivan Dubrovin <books.booksovich@yandex.com>
2025-04-07 10:05:40 +0300

    Add easier pane navigation to WezTerm

--- wezterm/keymap.lua
+++ wezterm/keymap.lua
@@ -8,6 +8,10 @@ function module.apply(config)
         { key = "|", mods = "LEADER", action = wezterm.action.SplitHorizontal },
         { key = "-", mods = "LEADER", action = wezterm.action.SplitVertical },
         { key = "z", mods = "LEADER", action = wezterm.action.TogglePaneZoomState },
+        { key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") },
+        { key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down") },
+        { key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up") },
+        { key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Right") },
         { key = "w", mods = "LEADER", action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
         { key = "d", mods = "LEADER", action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|DOMAINS" }) },
         {
