e610d3d21c587b85eb3ab2a82fc2ba66561a7143
Ivan Dubrovin <books.booksovich@yandex.com>
2025-08-17 11:25:35 +0300

    Add another server as an SSH domain

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -66,6 +66,12 @@ table.insert(config.hyperlink_rules, {
 config.ssh_domains = wezterm.default_ssh_domains()
 
 table.insert(config.ssh_domains, {
+    name = "vla5",
+    remote_address = "sg-sadr-perception-005.sas.yp-c.yandex.net",
+    username = "ivandubrovin",
+    remote_wezterm_path = "/home/ivandubrovin/.local/bin/wezterm",
+})
+table.insert(config.ssh_domains, {
     name = "dev",
     remote_address = "sg-sadr-perception-006.vla.yp-c.yandex.net",
     username = "ivandubrovin",
