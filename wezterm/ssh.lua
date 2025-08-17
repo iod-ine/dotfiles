9c7b930cb5d8a82ac664b86ca54206ad420fa3a1
Ivan Dubrovin <books.booksovich@yandex.com>
2025-08-17 13:43:20 +0300

    Update SSH domain names

--- wezterm/ssh.lua
+++ wezterm/ssh.lua
@@ -4,20 +4,14 @@ local module = {}
 function module.apply(config)
     config.ssh_domains = wezterm.default_ssh_domains()
     table.insert(config.ssh_domains, {
-        name = "vla5",
+        name = "sas5",
         remote_address = "sg-sadr-perception-005.sas.yp-c.yandex.net",
         username = "ivandubrovin",
         remote_wezterm_path = "/home/ivandubrovin/.local/bin/wezterm",
     })
     table.insert(config.ssh_domains, {
-        name = "dev",
+        name = "vla6",
         remote_address = "sg-sadr-perception-006.vla.yp-c.yandex.net",
-        username = "ivandubrovin",
-        remote_wezterm_path = "/home/ivandubrovin/.local/bin/wezterm",
-    })
-    table.insert(config.ssh_domains, {
-        name = "sas-dev",
-        remote_address = "mg-sadr-perception-001.sas.yp-c.yandex.net",
         username = "ivandubrovin",
         remote_wezterm_path = "/home/ivandubrovin/.local/bin/wezterm",
     })
