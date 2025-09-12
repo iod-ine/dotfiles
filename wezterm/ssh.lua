6472183115931d9b9af250a9faf174d65f7c05ac
Ivan Dubrovin <books.booksovich@yandex.com>
2025-09-12 10:27:55 +0300

    Switch to using hosts from ssh config

--- wezterm/ssh.lua
+++ wezterm/ssh.lua
@@ -4,20 +4,17 @@ function module.apply(config)
     config.ssh_domains = {}
     table.insert(config.ssh_domains, {
         name = "sas4",
-        remote_address = "sg-sadr-perception-004.sas.yp-c.yandex.net",
-        username = "ivandubrovin",
+        remote_address = "sas4",
         remote_wezterm_path = "/home/ivandubrovin/.local/bin/wezterm",
     })
     table.insert(config.ssh_domains, {
         name = "sas5",
-        remote_address = "sg-sadr-perception-005.sas.yp-c.yandex.net",
-        username = "ivandubrovin",
+        remote_address = "sas5",
         remote_wezterm_path = "/home/ivandubrovin/.local/bin/wezterm",
     })
     table.insert(config.ssh_domains, {
         name = "vla6",
-        remote_address = "sg-sadr-perception-006.vla.yp-c.yandex.net",
-        username = "ivandubrovin",
+        remote_address = "vla6",
         remote_wezterm_path = "/home/ivandubrovin/.local/bin/wezterm",
     })
     table.insert(config.ssh_domains, {
