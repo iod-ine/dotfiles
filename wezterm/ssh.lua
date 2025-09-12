b5a17982abb377ac85712be907673a6b0acda583
Ivan Dubrovin <books.booksovich@yandex.com>
2025-09-12 10:21:05 +0300

    Add another SSH domain for work

--- wezterm/ssh.lua
+++ wezterm/ssh.lua
@@ -1,8 +1,13 @@
-local wezterm = require("wezterm")
 local module = {}
 
 function module.apply(config)
     config.ssh_domains = {}
+    table.insert(config.ssh_domains, {
+        name = "sas4",
+        remote_address = "sg-sadr-perception-004.sas.yp-c.yandex.net",
+        username = "ivandubrovin",
+        remote_wezterm_path = "/home/ivandubrovin/.local/bin/wezterm",
+    })
     table.insert(config.ssh_domains, {
         name = "sas5",
         remote_address = "sg-sadr-perception-005.sas.yp-c.yandex.net",
