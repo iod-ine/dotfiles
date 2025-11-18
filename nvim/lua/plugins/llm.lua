0e7134276a8e262c1ec182c482b6522deb6e740c
Ivan Dubrovin <books.booksovich@yandex.com>
2025-11-18 20:09:54 +0300

    Add custom adapters for YandexLM

--- nvim/lua/plugins/llm.lua
+++ nvim/lua/plugins/llm.lua
@@ -7,6 +7,38 @@ return {
                 model = "qwen3:8b",
             },
         },
+        adapters = {
+            http = {
+                yalm_deepseek_r1 = function()
+                    return require("codecompanion.adapters").extend("openai_compatible", {
+                        env = {
+                            url = "http://zeliboba.yandex-team.ru/balance/deepseek_r1",
+                            api_key = "cmd:cat ~/.local/tokens/yalm",
+                            chat_url = "/v1/chat/completions",
+                        },
+                        schema = {
+                            model = {
+                                default = "deepseek",
+                            },
+                        },
+                    })
+                end,
+                yalm_deepseek_v3 = function()
+                    return require("codecompanion.adapters").extend("openai_compatible", {
+                        env = {
+                            url = "http://zeliboba.yandex-team.ru/balance/communal-deepseek-v3-0324-in-yt",
+                            api_key = "cmd:cat ~/.local/tokens/yalm",
+                            chat_url = "/v1/chat/completions",
+                        },
+                        schema = {
+                            model = {
+                                default = "deepseek",
+                            },
+                        }
+                    })
+                end,
+            },
+        },
     },
     dependencies = {
         "nvim-lua/plenary.nvim",
