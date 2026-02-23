88fb61ab6f7560f3c0571f5b0837a4d564b53dab
Ivan Dubrovin <books.booksovich@yandex.com>
2026-02-23 11:06:43 +0300

    Add a command to count tickets awaiting response

---
+++ raycast/scripts/startrek-tickets-waiting-for-response.py
@@ -0,0 +1,44 @@
+#!/Users/ivandubrovin/.dotfiles/raycast/scripts/.venv/bin/python
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Tracker: response needed
+# @raycast.mode inline
+# @raycast.refreshTime 1h
+
+# Optional parameters:
+# @raycast.icon ⏳
+
+# Documentation:
+# @raycast.description Count the amount of tickets waiting a response from me.
+
+import os
+
+import dotenv
+import requests
+
+assert dotenv.load_dotenv()
+
+response = requests.post(
+    url="https://st-api.yandex-team.ru/v3/issues/_count",
+    headers={
+        "Authorization": f"OAuth {os.environ.get('TRACKER_TOKEN')}",
+        "Host": "st-api.yandex-team.ru",
+    },
+    json={
+        "filter": {
+            "pendingReplyFrom": "me()",
+            "resolution": "unresolved()",
+            "status": ["new", "open", "inProgress"],
+        },
+    },
+)
+
+assert response.ok
+
+count = response.json()
+
+if count == 0:
+    print("\033[1;32mNo tickets need a response.\033[0m")
+else:
+    print(f"{count} ticket[s] are waiting for a response.")
