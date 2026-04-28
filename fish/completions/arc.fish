4bced3fd539dfd9b87ae9f7855867f8224bf3449
Ivan Dubrovin <books.booksovich@yandex.com>
2026-04-28 09:29:13 +0300

    Update fish completions for arc

--- fish/completions/arc.fish
+++ fish/completions/arc.fish
@@ -1,7 +1,9 @@
 complete -c arc -f
 
-complete -c arc -n "__fish_use_subcommand" -a "checkout commit diff stash status"
+complete -c arc -n "__fish_use_subcommand" -a "branch checkout commit diff pull push rebase stash status"
 
 complete -c arc -n "__fish_seen_subcommand_from checkout" -a "trunk"
-complete -c arc -n "__fish_seen_subcommand_from diff" -l staged
+complete -c arc -n "__fish_seen_subcommand_from diff" -l "staged"
+complete -c arc -n "__fish_seen_subcommand_from rebase" -l "continue"
+complete -c arc -n "__fish_seen_subcommand_from rebase" -a "trunk"
 complete -c arc -n "__fish_seen_subcommand_from stash" -a "push pop"
