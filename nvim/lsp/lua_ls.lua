e68291f8922c473e711cb32e8a2094b07aceb774
Ivan Dubrovin <books.booksovich@yandex.com>
2026-05-07 15:21:38 +0300

    Add config for the Lua language server

---
+++ nvim/lsp/lua_ls.lua
@@ -0,0 +1,15 @@
+return {
+    cmd = { 'lua-language-server' },
+    filetypes = { 'lua' },
+    settings = {
+        Lua = {
+            diagnostics = { globals = { 'vim' } },
+            runtime = { version = 'LuaJIT' },
+            workspace = {
+                checkThirdParty = false,
+                library = { vim.env.VIMRUNTIME },
+            },
+            format = { defaultConfig = { align_continuous_rect_table_field = false } },
+        },
+    },
+}
