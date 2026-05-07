b4e5bb09a385869a2c5850b21fcc878c88edc03c
Ivan Dubrovin <books.booksovich@yandex.com>
2026-05-07 22:34:43 +0300

    Add initial setup of mini.nvim
    There is still a lot of work to do, but this already feels nice.

---
+++ nvim/plugin/mini.lua
@@ -0,0 +1,62 @@
+vim.pack.add({ 'https://github.com/nvim-mini/mini.nvim' })
+
+require('mini.basics').setup({
+    options = { basic = false },
+    mappings = { basic = false },
+    autocommands = { basic = true },
+})
+require('mini.clue').setup({
+    triggers = { { mode = { 'n', 'x' }, keys = '<Leader>' } },
+    clues = {
+        { mode = 'n', keys = '<Leader>b', desc = '+Buffers' },
+        { mode = 'n', keys = '<Leader>e', desc = '+Explore' },
+        { mode = 'n', keys = '<Leader>f', desc = '+Find' },
+        { mode = 'n', keys = '<Leader>t', desc = '+Toggle' },
+    },
+})
+require('mini.completion').setup()
+require('mini.cursorword').setup()
+require('mini.diff').setup({ view = { style = 'number' } })
+require('mini.extra').setup()
+require('mini.files').setup({ windows = { preview = true, width_preview = 50 } })
+require('mini.icons').setup()
+require('mini.indentscope').setup({ draw = { animation = require('mini.indentscope').gen_animation.none() } })
+require('mini.move').setup({
+    mappings = {
+        left = '<C-M-h>',
+        right = '<C-M-l>',
+        down = '<C-M-j>',
+        up = '<C-M-k>',
+        line_left = '<C-M-h>',
+        line_right = '<C-M-l>',
+        line_down = '<C-M-j>',
+        line_up = '<C-M-k>',
+    }
+})
+require('mini.pick').setup()
+require('mini.statusline').setup()
+require('mini.pairs').setup()
+require('mini.surround').setup()
+require('mini.tabline').setup()
+
+-- More information on mini.pick pickers:
+--   https://nvim-mini.org/mini.nvim/doc/mini-pick.html#minipick.builtin
+--   https://nvim-mini.org/mini.nvim/doc/mini-extra.html#miniextra.pickers
+
+-- Explore:
+vim.keymap.set('n', '<Leader>eD', '<Cmd>lua MiniFiles.open()<CR>', { desc = 'working directory' })
+vim.keymap.set('n', '<Leader>ed', '<Cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<CR>',
+    { desc = 'current file directory' })
+
+-- Find:
+vim.keymap.set('n', '<Leader>fb', '<Cmd>Pick buffers<CR>', { desc = 'buffers' })
+vim.keymap.set('n', '<Leader>ff', '<Cmd>Pick files<CR>', { desc = 'files' })
+vim.keymap.set('n', '<Leader>fh', '<Cmd>Pick help<CR>', { desc = 'help' })
+vim.keymap.set('n', '<Leader>fr', '<Cmd>Pick oldfiles<CR>', { desc = 'recent files' })
+
+-- Toggle:
+local toggle_local_cursorword = function()
+    vim.b.minicursorword_disable = not vim.b.minicursorword_disable
+end
+
+vim.keymap.set('n', '<Leader>tc', toggle_local_cursorword, { desc = 'cursorword' })
