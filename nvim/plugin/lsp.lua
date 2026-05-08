5714b6efe398db63ef7d978f81f44786b449fc14
Ivan Dubrovin <books.booksovich@yandex.com>
2026-05-08 17:41:18 +0300

    Move LSP keymaps into an autocommand

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -1,3 +1,18 @@
 vim.lsp.enable({ 'lua_ls' })
 
-vim.keymap.set('n', '<Leader>bf', vim.lsp.buf.format, { desc = 'format' })
+vim.diagnostic.config({ virtual_lines = false, virtual_text = true })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    desc = 'Create buffer-local LSP keymaps',
+    callback = function(args)
+        -- Buffers
+        vim.keymap.set('n', '<Leader>bf', vim.lsp.buf.format, { buf = args.buf, desc = 'format' })
+
+        -- Toggle
+        local toggle_lsp_diagnostics = function()
+            vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+        end
+
+        vim.keymap.set('n', '<Leader>tq', toggle_lsp_diagnostics, { buf = args.buf, desc = 'diagnostics' })
+    end,
+})
