64ed40e44732ac56d990eb9907d8047201c430b0
Ivan Dubrovin <books.booksovich@yandex.com>
2026-05-08 17:57:47 +0300

    Disable hover in ruff in favor of ty

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -9,14 +9,24 @@ vim.diagnostic.config({ virtual_lines = false, virtual_text = true })
 vim.api.nvim_create_autocmd('LspAttach', {
     desc = 'Create buffer-local LSP keymaps',
     callback = function(args)
-        -- Buffers
+        -- Keymaps
+        -- Keymaps: Buffers
         vim.keymap.set('n', '<Leader>bf', vim.lsp.buf.format, { buf = args.buf, desc = 'format' })
 
-        -- Toggle
+        -- Keymaps: Toggle
         local toggle_lsp_diagnostics = function()
             vim.diagnostic.enable(not vim.diagnostic.is_enabled())
         end
-
         vim.keymap.set('n', '<Leader>tq', toggle_lsp_diagnostics, { buf = args.buf, desc = 'diagnostics' })
+
+        -- Client-specific configuration
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client == nil then
+            return
+        end
+        if client.name == 'ruff' then
+            -- Use hover from ty instead of riff
+            client.server_capabilities.hoverProvider = false
+        end
     end,
 })
