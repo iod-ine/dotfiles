34296eb50247fd009b86303b5d085837b304262f
Ivan Dubrovin <books.booksovich@yandex.com>
2026-05-09 19:14:32 +0300

    Install flash.nvim

---
+++ nvim/plugin/flash.lua
@@ -0,0 +1,6 @@
+vim.pack.add({ 'https://github.com/folke/flash.nvim' })
+
+require('flash').setup()
+
+vim.keymap.set({ 'n', 'x', 'o' }, 'S', function() require("flash").treesitter() end, {})
+vim.keymap.set({ 'n', 'x', 'o' }, 's', function() require("flash").jump() end, {})
