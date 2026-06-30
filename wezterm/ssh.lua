adfb9380c99b07c05767be74bbc50b0e3fad86fa
Ivan Dubrovin <books.booksovich@yandex.com>
2026-06-30 15:31:21 +0300

    Simplify the Wezterm SSH domains config

--- wezterm/ssh.lua
+++ wezterm/ssh.lua
@@ -1,22 +1,23 @@
 local module = {}
 
 function module.apply(config)
-    config.ssh_domains = {}
-    table.insert(config.ssh_domains, {
-        name = "sas",
-        remote_address = "sas",
-        remote_wezterm_path = "/home/ivandubrovin/.local/bin/wezterm",
-    })
-    table.insert(config.ssh_domains, {
-        name = "vla",
-        remote_address = "vla",
-        remote_wezterm_path = "/home/ivandubrovin/.local/bin/wezterm",
-    })
-    table.insert(config.ssh_domains, {
-        name = "yandex-cloud-vm",
-        remote_address = "yc",
-        remote_wezterm_path = "/home/ivan/.local/bin/wezterm",
-    })
+    config.ssh_domains = {
+        {
+            name = "sas",
+            remote_address = "sas",
+            remote_wezterm_path = "/home/ivandubrovin/.local/bin/wezterm",
+        },
+        {
+            name = "vla",
+            remote_address = "vla",
+            remote_wezterm_path = "/home/ivandubrovin/.local/bin/wezterm",
+        },
+        {
+            name = "yandex-cloud-vm",
+            remote_address = "yc",
+            remote_wezterm_path = "/home/ivan/.local/bin/wezterm",
+        },
+    }
 end
 
 return module
