869ea3bcf2812c72d27f482b6ea8c5c3279bc5ba
Ivan Dubrovin <books.booksovich@yandex.com>
2026-06-30 14:24:44 +0300

    Slighlty refactor the watch-nirvana-graph script

--- raycast/scripts/watch-nirvana-graph.py
+++ raycast/scripts/watch-nirvana-graph.py
@@ -21,17 +21,10 @@ import requests
 from loguru import logger
 
 
-logger.add("/tmp/watch-nirvana-graph.log", level="INFO")
-
-
-class TimeoutError(Exception):
-    pass
-
-
 def parse_nirvana_url_from_clipboard() -> tuple[str, str]:
+    """Parse a Nirvana graph URL from clipboards into workflow and instance IDs."""
     url = pyperclip.paste()
     regex = re.compile(r"nirvana.yandex-team.ru/flow/([^/]+)/([^/]+)/graph")
-
     return regex.findall(url)[0]
 
 
@@ -63,6 +56,8 @@ def watch_instance(
     delay: int = 60,
     timeout: int = 21600,
 ) -> str:
+    """Watch a running Nirvana graph instance and get its status when it finishes."""
+    logger.add("/tmp/watch-nirvana-graph.log", level="INFO", rotation="10 MB")
     logger.info(f"Started watching graph with {instance_id=}")
     start_time = time.time()
     while time.time() - start_time < timeout:
@@ -89,22 +84,24 @@ def notify(message: str):
 
 
 if __name__ == "__main__":
-    assert dotenv.load_dotenv()
-    try:
-        workflow_id, instance_id = parse_nirvana_url_from_clipboard()
-    except IndexError:
-        print("Could not parse the Nirvana URL.")
+    if not dotenv.load_dotenv():
+        print("Could not load the .env file.")
         sys.exit(1)
-
     if len(sys.argv) > 1 and sys.argv[1] == "--run-background":
         try:
+            workflow_id, instance_id = sys.argv[2], sys.argv[3]
             status = watch_instance(workflow_id, instance_id)
             notify(f"Graph finished with status '{status}'")
         except TimeoutError:
             notify("Timeout exceeded. No longer watching the instance.")
     else:
+        try:
+            workflow_id, instance_id = parse_nirvana_url_from_clipboard()
+        except IndexError:
+            print("Could not parse the Nirvana URL.")
+            sys.exit(1)
         subprocess.Popen(
-            [sys.executable, __file__, "--run-background"],
+            [sys.executable, __file__, "--run-background", workflow_id, instance_id],
             stdout=subprocess.DEVNULL,
             stderr=subprocess.DEVNULL,
             stdin=subprocess.DEVNULL,
