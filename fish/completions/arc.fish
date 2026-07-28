e348a3ed368425faf337e0ca49c7bd5d8404a5fa
Ivan Dubrovin <books.booksovich@yandex.com>
2026-07-28 21:23:44 +0300

    Improve completions for arc

--- fish/completions/arc.fish
+++ fish/completions/arc.fish
@@ -3,9 +3,9 @@ complete -c arc -f
 complete -c arc -n "__fish_use_subcommand" -a "add branch checkout commit diff mv pull push rebase stash status"
 
 complete -c arc -n "__fish_seen_subcommand_from add" -F
-complete -c arc -n "__fish_seen_subcommand_from checkout" -a "trunk"
+complete -c arc -n "__fish_seen_subcommand_from checkout" -a "(arc branch | sed -e 's/* //' -e 's/  //')"
 complete -c arc -n "__fish_seen_subcommand_from diff" -l "staged"
 complete -c arc -n "__fish_seen_subcommand_from mv" -F
 complete -c arc -n "__fish_seen_subcommand_from rebase" -a "trunk"
-complete -c arc -n "__fish_seen_subcommand_from rebase" -l "continue"
+complete -c arc -n "__fish_seen_subcommand_from rebase" -l "abort" -l "continue"
 complete -c arc -n "__fish_seen_subcommand_from stash" -a "push pop"
